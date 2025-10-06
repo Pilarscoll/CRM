a6fe5fde033bcaf6481f375b014e617db4a5cd65
Pili <scollopilar46@gmail.com>
2025-10-06 11:25:45 -0300

    cors

--- routes/usuarios_routes.js
+++ routes/usuarios_routes.js
@@ -3,7 +3,13 @@ const router = express.Router();
 const usuarioController = require('../controllers/usuarios_controllers');
 const authMiddleware = require('../middleware/auth'); // opcional, para proteger rutas
 const requireMiddleware = require('../middleware/requireAdmin'); // opcional, para proteger rutas
+import cors from "cors";
 
+app.use(cors({
+  origin: "http://localhost:5173", // tu frontend local
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  credentials: true, // si usás cookies o headers de auth
+}));
 // Rutas públicas
 router.post('/login', usuarioController.login); // login sin token
 router.post('/', usuarioController.create);    // crear usuario (podés protegerla si solo admin puede crear)
